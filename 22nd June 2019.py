733c15a98d18b57cea170008a68df56f5b1d6984
Skullhunter <43104519+skullhunter699@users.noreply.github.com>
2019-06-22 11:14:03 +0530

    New python Files

---
+++ 22nd June 2019.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=10
+if(x<15):
+    print("Good Morning")
+else:
+    print("Good Afternoon")
+
+
+# In[1]:
+
+
+x=10
+y=15
+if(x>y):
+    print("x is greater")
+else:
+    print("y is greater")
+
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[9]:
+
+
+x=int(input ("enter a number"))
+y=int(input ("enter a number"))
+if(x==y):
+    print(x*x)
+else:
+    print(x*y)
+
+
+# In[10]:
+
+
+x=-10
+if(x<0):
+    print("It is a negative number")
+elif(x>0):
+    print("It is a positive number")
+elif(x==0):
+    print("It is zero")
+        
+
+
+# In[11]:
+
+
+n=1;
+while(n<=10):
+    print(n)
+    n=n+1
+
+
+# In[14]:
+
+
+n=10;
+while(n>=1):
+    print(n)
+    n=n-1
+
+
+# In[15]:
+
+
+n=-22
+while(n>=-45):
+    print(n)
+    n=n-1
+
+
+# In[ ]:
+
+
+
+
